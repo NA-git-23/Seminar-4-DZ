8f263fe241510c4c115872ebd626e3edca02b9cc
NA <nabdraim@gmail.com>
2023-02-08 12:12:25 +0600

    init

---
+++ Task_25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+   if (a > 0)
+    {
+        double result = Math.Pow(a, b);
+        Console.WriteLine($"Число{a} ^ {b} степени = {result}");
+    }
+    
+    else if (a < 0 || (b % 2)==0)
+   
+        {
+            double result = Math.Pow(a, b);
+            Console.WriteLine($"Число{a} ^ {b} степени = {result}");
+        }
+            else
+            {
+                double result = Math.Pow(a, b);
+                Console.WriteLine($"Число -{a} ^ {b} степени = -{result}");
+            }
+    
+Console.WriteLine();
---
+++ Task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+//6, 1, 33 -> [6, 1, 33]
+
+
+//static void Main(int[] args)
+        {
+            int [] arr = new int[8] {1};
+           Console.ReadLine();
+            
+            for (int i=0; i<arr.Length; i++)
+            {
+                Console.Write($"Введите элементы массива:  \t ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadLine();
+        }
